ae6b14fbffb77e44f54ddfafb114e3759afb8c7d
James Han <james@checkbox.ai>
2019-10-09 03:30:28 +1100

    Added test files to test both loops & tables

--- es6/tests/checkbox-loops.js
+++ es6/tests/checkbox-loops.js
@@ -11,15 +11,29 @@ const {
 
 describe("Checkbox Loops", () => {
   it("should work with a simple loop", () => {
-    const doc = createDoc("checkbox-loops.docx");
+
+    /** TEST WITH JUST LOOPS */
+    // const doc = createDoc("checkbox-loops.docx");
+
+    // doc.setData({
+    //   num: 10,
+    //   num2: 15,
+    //   list1: ["foo", "bar", "heyhey"],
+    //   list2: [1, 2, 3],
+    //   "list2:formatted": ["1.000", "2.000", "3.000"],
+    //   list3: [10, 20, 30],
+    // });
+
+    /**** TEST WITH TABLES AS WELL */
+    const doc = createDoc("test_doc.docx")
 
     doc.setData({
-      num: 10,
-      num2: 15,
-      list1: ["foo", "bar", "heyhey"],
-      list2: [1, 2, 3],
-      "list2:formatted": ["1.000", "2.000", "3.000"],
-      list3: [10, 20, 30],
+      Text: "Static Text",
+      Num: 1,
+      "Num:formatted": "1.00",
+      NumList1: [5, 105],
+      TBL5_B1: [10, 100],
+      "TBL5_B1:formatted": ["10.00", "10000%"],
     });
 
     doc.setOptions({ delimiters: { start: "{{", end: "}}" }, parser: angularParser });
